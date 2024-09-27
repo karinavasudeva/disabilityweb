0078bb1d4188bae26c00c6ed25417b17452db87a
Karina Vasudeva <68547277+karinavasudeva@users.noreply.github.com>
2024-09-26 21:38:18 -0400

    Delete web directory

--- web/src/app/index.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import Home from './page'
-
-const IndexPage: React.FC = () => {
-  return <Home />
-}
-
-export default IndexPage
--- web/src/components/MotionDiv.tsx
+++
@@ -1,7 +0,0 @@
-'use client'
-
-import { motion } from 'framer-motion';
-
-const MotionDiv = motion.div;
-
-export default MotionDiv;
